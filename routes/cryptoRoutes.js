a6ed48e55a3b31b5da9c2be68909db4c04a47cb7
NIKUNJ KHINCHI <nikunj@dhruvtara.tech>
2024-10-09 20:30:28 +0530

    healthcheck added

--- routes/cryptoRoutes.js
+++ routes/cryptoRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.get('/stats', getStats);
 router.get('/deviation', getDeviation);
+router.get('/healthcheck', (req, res) => {
+    res.json({ status: 'ok' });
+});
 
 module.exports = router;
