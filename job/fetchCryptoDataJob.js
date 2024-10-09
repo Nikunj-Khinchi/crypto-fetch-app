d6724b03a8fc6a0142f22c1f0edec8a91577fb7a
NIKUNJ KHINCHI <nikunj@dhruvtara.tech>
2024-10-09 20:14:36 +0530

    file updated

--- job/fetchCryptoDataJob.js
+++ job/fetchCryptoDataJob.js
@@ -2,11 +2,11 @@ const cron = require('node-cron');
 const fetchCryptoData = require('../services/fetchCryptoService');
 const logger = require('../utils/logger');
 
-// // Run the fetch function immediately when the application starts
-// (async () => {
-//     logger.info('Running fetchCryptoData for the first time on startup...');
-//     await fetchCryptoData();
-// })();
+// Run the fetch function immediately when the application starts
+(async () => {
+    logger.info('Running fetchCryptoData for the first time on startup...');
+    await fetchCryptoData();
+})();
 
 
 // Schedule the cron job to run every 2 hours
